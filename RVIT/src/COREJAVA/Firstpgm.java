a657f0640dec5460498ea37a8a987ea3854385c5
Nagesh <nageshvnkt@gmail.com>
2023-12-20 19:59:51 +0530

    First Version

---
+++ RVIT/src/COREJAVA/Firstpgm.java
@@ -0,0 +1,9 @@
+package COREJAVA;
+
+public class Firstpgm {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ RVIT/src/COREJAVA/SecondPgm.java
@@ -0,0 +1,10 @@
+package COREJAVA;
+
+public class SecondPgm {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
